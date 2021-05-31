f0de967fbd16f55bec8aaafd841061d931d2fa40
Meghan Denny <hello@nektro.net>
2021-05-31 13:57:23 -0700

    run- ensure all data arrays are valid code

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,11 @@ const ucd = @import("unicode-ucd");
 pub fn main() !void {
     std.log.info("All your codebase are belong to us.", .{});
 
-    for (ucd.blocks.blocks) |b| {
-        std.debug.print("{}\n", .{b});
+    const data = .{
+        ucd.blocks.blocks,
+    };
+
+    inline for (data) |b| {
+        _ = b;
     }
 }
