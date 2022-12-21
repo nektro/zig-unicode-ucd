e84adca8b9512973e4e436dd4ec9db3c65d07823
Meghan Denny <hello@nektro.net>
2022-12-21 12:59:59 -0800

    main: run tests in order

--- src/main.zig
+++ src/main.zig
@@ -6,9 +6,11 @@ pub fn main() !void {
     std.log.info("All your codebase are belong to us.", .{});
 
     const data = .{
-        ucd.blocks,
         ucd.arabic_shaping,
         ucd.bidi_brackets,
+        ucd.bidi_mirroring,
+        ucd.blocks,
+        ucd.cjk_radicals,
     };
 
     inline for (data) |b| {
