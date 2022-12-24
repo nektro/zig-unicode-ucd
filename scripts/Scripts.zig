efca9d55662b8bcee7e804672a87fb768b9a922c
Meghan Denny <hello@nektro.net>
2022-12-24 15:54:50 -0800

    use common.RangeEnum in more places

--- scripts/Scripts.zig
+++ scripts/Scripts.zig
@@ -179,21 +179,5 @@ pub usingnamespace common.Main(struct {
         \\
     ;
 
-    pub fn exec(alloc: std.mem.Allocator, line: []const u8, writer: anytype) !bool {
-        _ = alloc;
-        var it = std.mem.tokenize(u8, line, "; ");
-
-        const first = it.next().?;
-        const next = it.next().?;
-
-        if (std.mem.indexOf(u8, first, "..")) |index| {
-            const start = first[0..index];
-            const end = first[index + 2 ..];
-            try writer.print("    .{{ .from = 0x{s}, .to = 0x{s}, .script = .{s} }},\n", .{ start, end, next });
-        } else {
-            try writer.print("    .{{ .from = 0x{s}, .to = 0x{s}, .script = .{s} }},\n", .{ first, first, next });
-        }
-
-        return true;
-    }
+    pub usingnamespace common.RangeEnum("script");
 });
