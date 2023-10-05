5177b94144ce9503c1ecca9c4978d9ae148f3218
Meghan Denny <hello@nektro.net>
2023-10-04 23:57:36 -0700

    PropertyAliases: missed some

--- scripts/PropertyAliases.zig
+++ scripts/PropertyAliases.zig
@@ -19,11 +19,20 @@ pub usingnamespace common.Main(struct {
 
     pub fn exec(alloc: std.mem.Allocator, line: []const u8, writer: anytype) !void {
         _ = alloc;
-        var it = std.mem.tokenize(u8, line, "; ");
+        const end = std.mem.indexOfScalar(u8, line, '#') orelse line.len;
+        var it = std.mem.tokenize(u8, line[0..end], "; ");
 
+        const short = it.next().?;
+        const long = it.next().?;
         try writer.print("    .{{ \"{}\", \"{}\" }},\n", .{
-            std.zig.fmtEscapes(it.next().?),
-            std.zig.fmtEscapes(it.next().?),
+            std.zig.fmtEscapes(short),
+            std.zig.fmtEscapes(long),
         });
+        while (it.next()) |more| {
+            try writer.print("    .{{ \"{}\", \"{}\" }},\n", .{
+                std.zig.fmtEscapes(more),
+                std.zig.fmtEscapes(long),
+            });
+        }
     }
 });
